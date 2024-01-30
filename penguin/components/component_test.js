e95254fdfdee1bf5ee974faaba999a3d453ae58e
Jack Coyne <157849101+BlazikenAlmighty@users.noreply.github.com>
2024-01-30 17:11:52 -0500

    Update component_test.js

--- penguin/components/component_test.js
+++ penguin/components/component_test.js
@@ -3,7 +3,7 @@ import { StyleSheet, Image, View, Text } from 'react-native';
 export default function Component_test() {
   return (
     <View>
-      <Text>Component test text is ere</Text>
+      <Text>Component test text is here</Text>
     </View>
   );
 }
