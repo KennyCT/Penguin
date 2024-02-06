0fb9aac9fa1470d058216df821adc2df3906fcbe
Henry <henrygjack@gmail.com>
2024-02-06 16:10:20 -0500

    fixes to index

--- penguin/app/(tabs)/index.tsx
+++ penguin/app/(tabs)/index.tsx
@@ -4,15 +4,15 @@ import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 //import * as database from '../../database.js'
 //import * as database from '../../database3'
-import { UserRepository, PublicUser } from '@/database3';
+//import { UserRepository, PublicUser } from '@/database3';
 //import {user} from '@/database3';
 
-var user : PublicUser = new UserRepository();
+//var user : PublicUser = new UserRepository();
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One Lol {user.fart()} 2</Text>
+      <Text style={styles.title}>Tab One Lol 2</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
