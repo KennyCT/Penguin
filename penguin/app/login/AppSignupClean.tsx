b73eccaa7c64363385d6ad7e4436e88a1ac3c7f5
King Matthew <mattshoup11@gmail.com>
2024-03-19 12:59:50 -0400

    AppSignupClean nearly done
    AppSignupClean has been polished and is close to completion. The only sticking points are how it works on iphone vs android and if each box element is centered perfectly.

--- penguin/app/login/AppSignupClean.tsx
+++ penguin/app/login/AppSignupClean.tsx
@@ -6,6 +6,8 @@ import { Link, router } from 'expo-router';
 import React, { useCallback, useState } from 'react'
 import { TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { useFonts } from 'expo-font';
+import Apploading from 'expo-app-loading'
 
 
 export default function SignupScreen() {
@@ -13,19 +15,28 @@ export default function SignupScreen() {
     const [form, setForm] = useState({
         email: '',
         password: '',
-    })
+    });
+
+    let [fontsLoaded] = useFonts({
+        'Lobster-Regular': require('./../../assets/fonts/Lobster1.4.otf')
+    });
+    if (!fontsLoaded){
+        return <Apploading/>;
+    }
 
     return(
 
         <View style={{ flex: 1 }}>
+
             <LinearGradient
-                colors={['white', '#004C99', '#000']}
+                // the only reason you only see 2 colors blend is because the box is so large that
+                // it does not fit entire blend on screen
+                colors={['#004C99', 'skyblue', 'white']} 
                 style={styles.entireScreen}>
             </LinearGradient>
 
-
             <View style={styles.penguinBox}>
-                <Text style={styles.penguinBoxText}>PENGUIN</Text>
+                <Text style={styles.penguinBoxText}>Penguin</Text>
             </View>
 
             <View style={styles.emailBox}>
@@ -52,7 +63,20 @@ export default function SignupScreen() {
                     value = {form.password}
                     onChangeText={password => setForm({...form, password})}>
                 </TextInput>
-                
+            </View>
+
+            <View style={styles.signUpBox}>
+                <TouchableOpacity onPress={() => {Alert.alert("Successful Sign Up")}}>
+                    <View style={styles.signUpBoxBtn}>
+                        <Text style={styles.signUpBoxText}>Sign Up</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.loginBox}>
+                <TouchableOpacity onPress={() => {Alert.alert("Successful Login")}}>
+                    <Text style={styles.loginBoxText}>Have an account? Click here to Login</Text>
+                </TouchableOpacity>
             </View>
 
         </View>
@@ -69,18 +93,23 @@ const styles = StyleSheet.create({
     },
     emailANDpassBox: {
         backgroundColor: '#E7EEF5',
+        borderColor: 'lightblue',
+        borderWidth: 1,
         borderRadius: 12,
         paddingVertical: 8,
         paddingHorizontal: 10,
         //marginBottom: 30,
+        width: 275,
+        height: 30,
+        left: 10
     },
     penguinBox: {
-        backgroundColor: 'white', 
+        backgroundColor: 'transparent', 
         width: 300, 
         height: 70, 
         // paddingHorizontal: 100,
         // paddingVertical: 100,
-        top: 100,
+        top: 110,
         left: 50,
         //right: 50,
         justifyContent: 'center', 
@@ -90,35 +119,85 @@ const styles = StyleSheet.create({
     penguinBoxText: {
         fontSize: 46,
         fontWeight: '800',
-        color: '#2881D8'
+        color: 'white',
+        fontFamily: 'Lobster-Regular'
     },
     emailBox: {
-        backgroundColor: 'white', 
+        backgroundColor: 'transparent', 
         width: 300, 
-        height: 120, 
-        top: 190,
+        height: 100, 
+        top: 240,
         left: 50,
         justifyContent: 'center', 
-        position: 'absolute'
+        position: 'absolute',
+        //opacity: .5
+        // borderTopColor: '#2881D8',
+        // borderLeftColor: '#2881D8',
+        // borderRightColor: '#2881D8',
+        // borderBottomColor: 'white',
+        // borderWidth: 5
     },
     emailBoxText: {
         fontSize: 36,
         fontWeight: '800',
-        color: '#2881D8'
+        color: 'white',
+        left: 10
     },
     passwordBox: {
-        backgroundColor: 'white', 
+        backgroundColor: 'transparent', 
         width: 300, 
         height: 100, 
-        top: 310,
+        top: 330,
         left: 50,
         justifyContent: 'center', 
-        position: 'absolute'
+        position: 'absolute',
     },
     passwordBoxText: {
         fontSize: 36,
         fontWeight: '800',
-        color: '#2881D8'
+        color: 'white',
+        left: 10
     },
+    signUpBox: {
+        backgroundColor: 'transparent', 
+        width: 300, 
+        height: 100, 
+        top: 430,
+        left: 50,
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        position: 'absolute',
+    },
+    signUpBoxBtn: {
+        backgroundColor: '#2881D8',
+        borderColor: '#2881D8',
+        borderRadius: 30,
+        //flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 7,
+        paddingHorizontal: 70,
+    },
+    signUpBoxText: {
+        fontSize: 30,
+        fontWeight: '800',
+        color: 'white'
+    },
+    loginBox: {
+        backgroundColor: 'white', 
+        width: 250, 
+        height: 30, 
+        top: 620,
+        left: 75,
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        position: 'absolute',
+        borderRadius: 30
+    },
+    loginBoxText: {
+        fontSize: 10,
+        fontWeight: '800',
+        color: '#2881D8'
+    }
 
   });
